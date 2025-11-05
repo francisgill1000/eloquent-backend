c75bf160a0b6933175966354c90010a91ffdf99d
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2025-11-06 00:13:01 +0400

    added customer seeder

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\InvoiceReminderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {       
     return $request->user();
 })->middleware('auth:sanctum');
 
