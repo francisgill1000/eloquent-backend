0a09541df9dd51806fcd7b66eb7bc7b66d9d7f2e
francisgill1000 <50021515+francisgill1000@users.noreply.github.com>
2026-06-12 00:40:23 +0400

    feat(wa): web-push subscriptions + notify service (bizrezzy subscribes later)

--- routes/api.php
+++ routes/api.php
@@ -112,6 +112,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/master/bot-prompts/{botPrompt}', [\App\Http\Controllers\MasterController::class, 'updateBotPrompt']);
     Route::delete('/master/bot-prompts/{botPrompt}', [\App\Http\Controllers\MasterController::class, 'deleteBotPrompt']);
     Route::post('/master/bot-prompts/{botPrompt}/activate', [\App\Http\Controllers\MasterController::class, 'activateBotPrompt']);
+    Route::get('/wa/push/vapid-key', [\App\Http\Controllers\WaPushController::class, 'vapidKey']);
+    Route::post('/wa/push/subscribe', [\App\Http\Controllers\WaPushController::class, 'subscribe']);
+    Route::post('/wa/push/unsubscribe', [\App\Http\Controllers\WaPushController::class, 'unsubscribe']);
     Route::post('/shop/category', [ShopController::class, 'confirmCategory']);
     Route::get('/shop/wa/account', [\App\Http\Controllers\WaChatController::class, 'account']);
     Route::post('/shop/wa/account', [\App\Http\Controllers\WaChatController::class, 'saveAccount']);
